7d310ac38c26e6806cc244d2370de205f2ce58b4
athul1207 <athulsunil1207@gmail.com>
2024-10-09 10:40:48 +0530

    version2

--- file.py
+++ file.py
@@ -5,6 +5,8 @@ num2 = 16
 # Calculating square roots without using a function
 sqrt_num1 = num1 ** 0.5
 sqrt_num2 = num2 ** 0.5
+sqrt_num3 = num3 ** 0.5
 
 print(f"The square root of {num1} is {sqrt_num1}")
+print(f"The square root of {num2} is {sqrt_num2}")
 print(f"The square root of {num2} is {sqrt_num2}")
